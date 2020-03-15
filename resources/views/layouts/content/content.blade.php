f3da33070fde59d5847d349503f4c464106ac7d8
Syamil <syamil@codelabs.co.id>
2020-03-16 05:55:26 +0700

    init

--- resources/views/layouts/content/content.blade.php
+++
@@ -1,8 +0,0 @@
-        <div class="container-fluid">
-
-          <!-- Page Heading -->
-          @yield('heading')
-
-          <!-- Content Row -->
-          @yield('wrap_content')
-        </div>
