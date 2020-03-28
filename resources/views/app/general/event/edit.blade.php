d848b5c28cdef2ab9292491d08e988aba06356a8
Rakha <rakha@codelabs.co.id>
2020-03-29 03:07:22 +0700

    mengubah update event

--- resources/views/app/general/event/edit.blade.php
+++ resources/views/app/general/event/edit.blade.php
@@ -60,6 +60,7 @@
               <input type="text" class="form-control" value="{{$edit['location']}}">
             </div>
           </div>
+          <input type="hidden" name="company_id" value="{{$edit['company_id']}}" disabled>
           <div class="form-group row mb-4">
             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
             <div class="col-sm-12 col-md-7">
