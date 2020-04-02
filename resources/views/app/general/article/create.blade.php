33c612ef0f6d0c934b28867d002b3c04c43b1243
Abimanyu Wima Putra <kyoabhiem@gmail.com>
2020-04-02 21:31:13 +0700

    update article

--- resources/views/app/general/article/create.blade.php
+++ resources/views/app/general/article/create.blade.php
@@ -3,85 +3,118 @@
 @section('title','Forum | Article')
 
 @section('section_header')
-<h1>Create Post</h1>
-<div class="section-header-breadcrumb">
-  <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-  <div class="breadcrumb-item"><a href="#">General</a></div>
-  <div class="breadcrumb-item">CreateArticle</div>
-</div>
+    <h1>Create Post</h1>
+    <div class="section-header-breadcrumb">
+        <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+        <div class="breadcrumb-item"><a href="#">General</a></div>
+        <div class="breadcrumb-item">CreateArticle</div>
+    </div>
 @endsection
 
 @section('wrap_content')
-<div class="row">
-  <div class="col-12">
-    <div class="card">
-      <div class="card-header">
-        <h4>Write Your Post</h4>
-      </div>
-      <div class="card-body">
-        <form method="POST" action="{{  url('general/article/store') }}" class="needs-validation" novalidate="" enctype="multipart/form-data">
-          @csrf
-          <div class="form-group row mb-4">
-            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Title</label>
-            <div class="col-sm-12 col-md-7">
-              <input type="text" class="form-control" name="title">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Write Your Post</h4>
+                </div>
+                <div class="card-body">
+                    <form method="POST" action="{{  url('general/article/store') }}" class="needs-validation"
+                          novalidate="" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group row mb-4">
+                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Title</label>
+                            <div class="col-sm-12 col-md-7">
+                                <input type="text" class="form-control" name="title">
+                            </div>
+                        </div>
+                        <div class="form-group row mb-4">
+                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Category</label>
+                            <div class="col-sm-12 col-md-7">
+                                <select class="form-control selectric" name="category_id">
+                                    @foreach($categorys as $category)
+                                        <option value="{{ $category['id'] }}">{{ $category['category'] }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-4">
+                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Content</label>
+                            <div class="col-sm-12 col-md-7">
+                                <textarea class="summernote-simple" name="content">{{old('content')}}</textarea>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-4">
+                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Thumbnail</label>
+                            <div class="col-sm-12 col-md-7">
+                                <div id="image-preview" class="image-preview">
+                                    <label for="image-upload" id="image-label">Choose File</label>
+                                    <input type="file" name="image" id="image-upload"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-4">
+                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Tags</label>
+                            <div class="col-sm-12 col-md-7">
+                                <select name="tags[]" class="select2" style="width: 100%;" multiple="multiple"></select>
+                            </div>
+                        </div>
+                        <!-- <div class="form-group row mb-4">
+                          <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Status</label>
+                          <div class="col-sm-12 col-md-7">
+                            <select class="form-control selectric">
+                              <option>Publish</option>
+                              <option>Draft</option>
+                              <option>Pending</option>
+                            </select>
+                          </div>
+                        </div> -->
+                        <div class="form-group row mb-4">
+                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                            <div class="col-sm-12 col-md-7">
+                                <a href="{{url('general/article')}}" class="btn btn-secondary">Cancel</a>
+                                <button type="submit" class="btn btn-primary">Create Post</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-          </div>
-          <div class="form-group row mb-4">
-            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Category</label>
-            <div class="col-sm-12 col-md-7">
-              <select class="form-control selectric" name="category">
-                  <option value="review">Review</option>
-                  <option value="tips & trick">Tips & Trick</option>
-              </select>
-            </div>
-          </div>
-          <div class="form-group row mb-4">
-            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Content</label>
-            <div class="col-sm-12 col-md-7">
-              <textarea class="summernote-simple" name="content">{{old('content')}}</textarea>
-            </div>
-          </div>
-          <div class="form-group row mb-4">
-            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Thumbnail</label>
-            <div class="col-sm-12 col-md-7">
-              <div id="image-preview" class="image-preview">
-                <label for="image-upload" id="image-label">Choose File</label>
-                <input type="file" name="image" id="image-upload" />
-            </div>
-          </div>
-          </div>
-          <div class="form-group row mb-4">
-            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Tags</label>
-            <div class="col-sm-12 col-md-7">
-              <input type="text" class="form-control inputtags" name="tags" value="{{old('tags')}}">
-            </div>
-          </div>
-          <!-- <div class="form-group row mb-4">
-            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Status</label>
-            <div class="col-sm-12 col-md-7">
-              <select class="form-control selectric">
-                <option>Publish</option>
-                <option>Draft</option>
-                <option>Pending</option>
-              </select>
-            </div>
-          </div> -->
-          <div class="form-group row mb-4">
-            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
-            <div class="col-sm-12 col-md-7">
-              <a href="{{url('general/article')}}" class="btn btn-secondary">Cancel</a>
-              <button type="submit" class="btn btn-primary">Create Post</button>
-            </div>
-          </div>
-        </form>
-      </div>
+        </div>
     </div>
-  </div>
-</div>
-
 @endsection
 
 @section('script_page')
-<script src="{{ asset('stisla/assets/js/page/features-post-create.js') }}"></script>
+    <script>
+        $(".select2").select2({
+            tags: true
+        });
+        function formatStateResult(opt) {
+            if (!opt.id) {
+                return opt.text;
+            }
+            var DataImage = $(opt.element).data('image');
+            if (!DataImage) {
+                return opt.text;
+            } else {
+                var $opt = $(
+                    '<span><img class="rounded-circle" src="' + DataImage + '" alt="avatar" width="32px"> ' + opt.text + '</span>'
+                );
+                return $opt;
+            }
+        }
+        function formatState(opt) {
+            if (!opt.id) {
+                return opt.text;
+            }
+            var DataImage = $(opt.element).data('image');
+            if (!DataImage) {
+                return opt.text;
+            } else {
+                var $opt = $(
+                    '<span><img class="mr-2 rounded-circle" src="' + DataImage + '" alt="avatar" width="16px"> ' + opt.text + '</span>'
+                );
+                return $opt;
+            }
+        }
+    </script>
 @endsection
--- resources/views/app/general/article/detail.blade.php
+++ resources/views/app/general/article/detail.blade.php
@@ -3,81 +3,81 @@
 @section('title','Forum | Article')
 
 @section('section_header')
-<h1>Article</h1>
-<div class="section-header-breadcrumb">
-  <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-  <div class="breadcrumb-item"><a href="#">General</a></div>
-  <div class="breadcrumb-item">DetailArticle</div>
-</div>
+    <h1>Article</h1>
+    <div class="section-header-breadcrumb">
+        <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+        <div class="breadcrumb-item"><a href="#">General</a></div>
+        <div class="breadcrumb-item">DetailArticle</div>
+    </div>
 @endsection
 
 @section('wrap_content')
-<div class="row">
-  <div class="col-12">
-      <div class="card">
-        <div class="card-header">
-          <h4>Detail</h4>
-        </div>
-        <div class="card-body">
-          <div class="form-group row mb-4">
-            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3 offset-1">Title : </label>
-            <label class="col-form-label text-md-left col-12 col-md-3 col-lg-3">{{$article['title']}}</label>
-          </div>
-          <div class="form-group row mb-4">
-            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3 offset-1">Category : </label>
-            <!-- <div class="col-sm-12 col-md-7">
-              <select class="form-control selectric">
-                  <option value="review">Review</option>
-                  <option value="tips & trick">Tips & Trick</option>
-              </select>
-            </div> -->
-            <label class="col-form-label text-md-left col-12 col-md-3 col-lg-3">{{$article['category']}}</label>
-          </div>
-          <div class="form-group row mb-4">
-            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3 offset-1">Content : </label>
-            <!-- <div class="col-sm-12 col-md-7">
-              <textarea class="summernote-simple"></textarea>
-            </div> -->
-            <label class="col-form-label text-md-left col-12 col-md-3 col-lg-6"><?= $article['content']; ?></label>
-          </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3 offset-1">Thumbnail</label>
-                      <!-- <div class="col-sm-12 col-md-7">
-                        <div id="image-preview" class="image-preview">
-                          <label for="image-upload" id="image-label">Choose File</label>
-                          <input type="file" name="image" id="image-upload" />
-                        </div>
-                      </div> -->
-                      <div class="col-sm-12 col-md-7">
-                        <img src="{{$article['image']}}" alt="Thumbnail" style="height:270px">
-                      </div>
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3 offset-1">Tags : </label>
-                      <label class="col-form-label text-md-left col-12 col-md-3 col-lg-3">
-                        @if($article['tags'] != null)
-                          @foreach ($article['tags'] as $tag)
-                          <span class="badge badge-primary">{{ $tag }}</span>
-                          @endforeach
-                        @endif
-                      </label>
-                      <!-- <div class="col-sm-12 col-md-7">
-                        <input type="text" class="form-control inputtags">
-                      </div> -->
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3"></label>
-                      <div class="col-sm-12 col-md-7">
-                        <a href="{{url('general/article')}}" class="btn btn-secondary">Back</a>
-                      </div>
-                    </div>
-                  </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Detail</h4>
                 </div>
-              </div>
+                <div class="card-body">
+                    <div class="form-group row mb-4">
+                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3 offset-1">Title : </label>
+                        <label
+                            class="col-form-label text-md-left col-12 col-md-3 col-lg-3">{{$article['title']}}</label>
+                    </div>
+                    <div class="form-group row mb-4">
+                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3 offset-1">Category : </label>
+                        <!-- <div class="col-sm-12 col-md-7">
+                          <select class="form-control selectric">
+                              <option value="review">Review</option>
+                              <option value="tips & trick">Tips & Trick</option>
+                          </select>
+                        </div> -->
+                        <label class="col-form-label text-md-left col-12 col-md-3 col-lg-3">{{$article['category']['category']}}</label>
+                    </div>
+                    <div class="form-group row mb-4">
+                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3 offset-1">Content : </label>
+                        <!-- <div class="col-sm-12 col-md-7">
+                          <textarea class="summernote-simple"></textarea>
+                        </div> -->
+                        <label class="col-form-label text-md-left col-12 col-md-3 col-lg-6">{!! $article['content'] !!}</label>
+                    </div>
+                    <div class="form-group row mb-4">
+                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3 offset-1">Thumbnail</label>
+                        <!-- <div class="col-sm-12 col-md-7">
+                          <div id="image-preview" class="image-preview">
+                            <label for="image-upload" id="image-label">Choose File</label>
+                            <input type="file" name="image" id="image-upload" />
+                          </div>
+                        </div> -->
+                        <div class="col-sm-12 col-md-7">
+                            <img src="{{$article['image']}}" alt="Thumbnail" style="height:270px">
+                        </div>
+                    </div>
+                    <div class="form-group row mb-4">
+                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3 offset-1">Tags : </label>
+                        <label class="col-form-label text-md-left col-12 col-md-3 col-lg-3">
+                            @forelse ($article['tags'] as $tag)
+                                <span class="badge badge-primary">{{ $tag }}</span>
+                            @empty
+                            @endforelse
+                        </label>
+                        <!-- <div class="col-sm-12 col-md-7">
+                          <input type="text" class="form-control inputtags">
+                        </div> -->
+                    </div>
+                    <div class="form-group row mb-4">
+                        <label class="col-form-label text-md-right col-12 col-md-3"></label>
+                        <div class="col-sm-12 col-md-7">
+                            <a href="{{url('general/article')}}" class="btn btn-secondary">Back</a>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
+    </div>
 
 @endsection
 
 @section('script_page')
-<script src="{{ asset('stisla/assets/js/page/features-post-create.js') }}"></script>
+    <script src="{{ asset('stisla/assets/js/page/features-post-create.js') }}"></script>
 @endsection
