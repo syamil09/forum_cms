482692bcf5d13435ece9f1faacc667aca506b4ca
Syamil <syamil@codelabs.co.id>
2020-04-20 15:51:02 +0700

    perbaiki crud event

--- resources/views/app/general/event/create.blade.php
+++ resources/views/app/general/event/create.blade.php
@@ -12,6 +12,13 @@
 @endsection
 
 @section('wrap_content')
+{{-- Style Validation --}}
+    <style>
+        .is-invalid {
+            color: red;
+        }
+    </style>
+{{-- End Style Validation --}}
 <div class="row">
   <div class="col-12">
     <div class="card">
@@ -19,30 +26,30 @@
         <h4>Create Event</h4>
       </div>
       <div class="card-body">
-        <form class="needs-validation" action="{{url('general/event/store')}}" method="post" enctype="multipart/form-data">
+        <form id="createEvent" class="" action="{{url('general/event/store')}}" method="post" enctype="multipart/form-data">
           @csrf
           <div class="form-group row mb-4">
             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Title</label>
             <div class="col-sm-12 col-md-7">
-              <input type="text" class="form-control" name="title" value="{{old('title')}}">
+              <input type="text" class="form-control" name="title" value="{{old('title')}}" required>
             </div>
           </div>
           <div class="form-group row mb-4">
             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Event Start</label>
             <div class="col-sm-12 col-md-7">
-              <input type="date" class="form-control" name="event_start" value="{{old('event_start')}}">
+              <input type="date" class="form-control" name="event_start" value="{{old('event_start')}}" required>
             </div>
           </div>
           <div class="form-group row mb-4">
             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Event End</label>
             <div class="col-sm-12 col-md-7">
-              <input type="date" class="form-control" name="event_end" value="{{old('event_end')}}">
+              <input type="date" class="form-control" name="event_end" value="{{old('event_end')}}" required>
             </div>
           </div>
           <div class="form-group row mb-4">
             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Content</label>
             <div class="col-sm-12 col-md-7">
-              <textarea class="summernote-simple" name="description">{{old('description')}}</textarea>
+              <textarea class="summernote-simple" name="description" required>{{old('description')}}</textarea>
             </div>
           </div>
           <div class="form-group row mb-4">
@@ -50,22 +57,22 @@
             <div class="col-sm-12 col-md-7">
               <div id="image-preview" class="image-preview">
                 <label for="image-upload" id="image-label">Choose File</label>
-                <input type="file" name="image" id="image-upload" />
+                <input type="file" name="image[]" id="image-upload" multiple required />
               </div>
             </div>
           </div>
           <div class="form-group row mb-4">
             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Location</label>
             <div class="col-sm-12 col-md-7">
-              <input type="text" class="form-control" name="location" value="{{old('location')}}">
+              <input type="text" class="form-control" name="location" value="{{old('location')}}" required>
             </div>
           </div>
           <div class="form-group row mb-4">
               <div class="col-sm-12 col-md-4 offset-2">
-                <input type="text" name="latitude" class="form-control" placeholder="Latitude..." value="{{old('latitude')}}">
+                <input type="text" name="latitude" class="form-control" placeholder="Latitude..." value="{{old('latitude')}}" required>
               </div>
               <div class="col-sm-12 col-md-4">
-                <input type="text" name="longitude" class="form-control" placeholder="Longitude..." value="{{old('longitude')}}">
+                <input type="text" name="longitude" class="form-control" placeholder="Longitude..." value="{{old('longitude')}}" required>
               </div>
           </div>
           <div class="form-group row mb-4">
@@ -84,5 +91,35 @@
 @endsection
 
 @section('script_page')
-<script src="{{ asset('stisla/assets/js/page/features-post-create.js') }}"></script>
+{{-- Valiidatoor --}}
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/jquery.validate.min.js"></script>
+    <script>
+         $.validator.setDefaults({
+            errorElement: "span",
+            errorClass: "is-invalid",
+            //  validClass: 'stay',
+            highlight: function (element, errorClass, validClass) {
+                $(element).addClass(errorClass);
+                $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+            },
+            unhighlight: function (element, errorClass, validClass) {
+                $(element).removeClass(errorClass);
+                $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+            },
+            errorPlacement: function (error, element) {
+                if (element.parent('.input-group').length) {
+                    error.insertAfter(element.parent());
+                } else if (element.hasClass('select2')) {
+                    error.insertAfter(element.next('span'));
+                } else {
+                    error.insertAfter(element);
+                }
+            }
+        });
+        $('#createEvent').validate();
+        $('#createEvent').valid();
+    </script>
+    {{-- End Valiidatoor --}}
+<!-- <script src="{{ asset('stisla/assets/js/page/features-post-create.js') }}"></script> -->
 @endsection
+
