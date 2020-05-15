671b934f9873e9c2a27b67fd7d1ca2a262b17f21
Abimanyu Wima Putra <kyoabhiem@gmail.com>
2020-05-15 15:55:14 +0700

    update menu sidebar

--- app/Helpers/Menu.php
+++ app/Helpers/Menu.php
@@ -7,16 +7,32 @@ use Illuminate\Support\Facades\Session;
 
 class Menu extends Controller
 {
-    public function list()
+    public function list($sidebar = false)
     {
-        if (Cache::has('menu'))
-        {
-            return Cache::get('menu');
+        if (!Cache::has('menu')) {
+            $GroupMenus = Cache::remember('menu', 60, function () {
+                return $this->get(env('GATEWAY_URL') . 'cms/menu', Session::get('token'))['data'];
+            });
+        } else {
+            $GroupMenus = Cache::get('menu');
         }
-        Cache::remember('menu', 60, function () {
-            return $this->get(env('GATEWAY_URL') . 'cms/menu', Session::get('token'))['data'];
-        });
-        return Cache::get('menu');
+
+        if ($sidebar === true) {
+            $privilages = session()->get('privileges');
+            foreach ($GroupMenus as $iGM => $groupMenu) {
+                foreach ($groupMenu['menu'] as $iM => $menu) {
+                    $MenuPrivilages = collect($privilages)->where('menu_id', $menu['id'])->first();
+                    $GroupMenus[$iGM]['menu'][$iM]['view'] = $MenuPrivilages['view'];
+                    if ($MenuPrivilages['view'] == "1") {
+                        $GroupMenus[$iGM]['totalMenu'] = empty($GroupMenus[$iGM]['totalMenu']) ? 1 :$GroupMenus[$iGM]['totalMenu'] + 1;
+                    } else {
+                        $GroupMenus[$iGM]['totalMenu'] = 0;
+                    }
+                }
+            }
+        }
+        return $GroupMenus;
+
     }
 
 }
--- resources/views/layouts/content/sidebar.blade.php
+++ resources/views/layouts/content/sidebar.blade.php
@@ -1,70 +1,72 @@
 <div class="main-sidebar">
-  <aside id="sidebar-wrapper">
-    <div class="sidebar-brand">
-      <a href="index.html">Forum</a>
-    </div>
-    <div class="sidebar-brand sidebar-brand-sm">
-      <a href="index.html">St</a>
-    </div>
-    <ul class="sidebar-menu">
-      <li class="menu-header">Dashboard</li>
-      <li class=" active">
-        <a href="{{ url('/') }}" class="nav-link"><i class=" fas fa-fire"></i><span>Dashboard</span></a>
-      </li>
-      <li class="menu-header">Starter</li>
-        @php($GroupMenus = new \App\Helpers\Menu())
-        @foreach($GroupMenus->list() as $GroupMenu)
-            <li class="nav-item dropdown wrap-menu">
-                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"> {!! $GroupMenu['icon'] !!} <span>{{ $GroupMenu['name'] }}</span></a>
-                <ul class="dropdown-menu">
-                  @foreach($GroupMenu['menu'] as $key => $menu)
-                  <?php $menu_priv = collect(session()->get('privileges'))->where('menu_id',$menu['id'])->first(); ?>
-                  
-                    @if($menu_priv['view'] == "1")
-                    <li><a class="submenu nav-link" href="{{ url($menu['url']) }}">{{ $menu['name'] }}</a></li>
-                    @endif
-                  @endforeach
-  
-                </ul>
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Forum</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class=" active">
+                <a href="{{ url('/') }}" class="nav-link"><i class=" fas fa-fire"></i><span>Dashboard</span></a>
             </li>
-        @endforeach
-      {{--<li class="nav-item dropdown">--}}
-        {{--<a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-users"></i> <span>Manage--}}
+            <li class="menu-header">Starter</li>
+            @php($GroupMenus = new \App\Helpers\Menu())
+            @foreach($GroupMenus->list(true) as $GroupMenu)
+                @if($GroupMenu['totalMenu'] > 0)
+                <li class="nav-item dropdown wrap-menu">
+                    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"> {!! $GroupMenu['icon'] !!}<span>{{ $GroupMenu['name'] }}</span></a>
+                    <ul class="dropdown-menu">
+                        @foreach($GroupMenu['menu'] as $menu)
+                            @if($menu['view'] == 1)
+                                <li>
+                                    <a class="submenu nav-link" href="{{ url($menu['url']) }}">{{ $menu['name'] }}</a>
+                                </li>
+                            @endif
+                        @endforeach
+                    </ul>
+                </li>
+                @endif
+            @endforeach
+            {{--<li class="nav-item dropdown">--}}
+            {{--<a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-users"></i> <span>Manage--}}
             {{--Users</span></a>--}}
-        {{--<ul class="dropdown-menu">--}}
-          {{--<li><a class="nav-link" href="{{ url('account/user/') }}">Member Company</a></li>--}}
-          {{--<li><a class="nav-link" href="{{ url('account/admin/') }}">Admin Company</a></li>--}}
-        {{--</ul>--}}
-      {{--</li>--}}
-      {{--<li class="nav-item dropdown">--}}
-        {{--<a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>General</span></a>--}}
-        {{--<ul class="dropdown-menu">--}}
-          {{--<li><a class="nav-link" href="{{ url('general/article/') }}">Article</a></li>--}}
-          {{--<li><a class="nav-link" href="{{ url('general/article_category/') }}">Article Category</a></li>--}}
-          {{--<li><a class="nav-link" href="{{ url('general/event/') }}">Event</a></li>--}}
-          {{--<li><a class="nav-link" href="{{ url('general/vote') }}">Vote</a></li>--}}
-          {{--<!-- <li><a class="nav-link" href="{{ url('general/splashscreen') }}">Splash Screen</a></li> -->--}}
-          {{--<li><a class="nav-link" href="{{ url('general/walkthrough') }}">Walk Through</a></li>--}}
-          {{--<li><a class="nav-link" href="{{ url('general/highlight') }}">Highlight</a></li>--}}
-        {{--</ul>--}}
-      {{--</li>--}}
-      {{--<li class="nav-item dropdown">--}}
-        {{--<a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i> <span>Companies</span></a>--}}
-        {{--<ul class="dropdown-menu">--}}
-          {{--<li><a class="nav-link" href="{{url('company/community')}}">Community</a></li>--}}
-          {{--<li><a class="nav-link" href="{{url('company/secretariat')}}">Secretariat</a></li>--}}
-          {{--<li><a class="nav-link" href="{{url('company/shop')}}">Shop</a></li>--}}
-          {{--<li><a class="nav-link" href="{{url('company/store')}}">Store</a></li>--}}
-        {{--</ul>--}}
-      {{--</li>--}}
-    {{--</ul>--}}
+            {{--<ul class="dropdown-menu">--}}
+            {{--<li><a class="nav-link" href="{{ url('account/user/') }}">Member Company</a></li>--}}
+            {{--<li><a class="nav-link" href="{{ url('account/admin/') }}">Admin Company</a></li>--}}
+            {{--</ul>--}}
+            {{--</li>--}}
+            {{--<li class="nav-item dropdown">--}}
+            {{--<a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>General</span></a>--}}
+            {{--<ul class="dropdown-menu">--}}
+            {{--<li><a class="nav-link" href="{{ url('general/article/') }}">Article</a></li>--}}
+            {{--<li><a class="nav-link" href="{{ url('general/article_category/') }}">Article Category</a></li>--}}
+            {{--<li><a class="nav-link" href="{{ url('general/event/') }}">Event</a></li>--}}
+            {{--<li><a class="nav-link" href="{{ url('general/vote') }}">Vote</a></li>--}}
+            {{--<!-- <li><a class="nav-link" href="{{ url('general/splashscreen') }}">Splash Screen</a></li> -->--}}
+            {{--<li><a class="nav-link" href="{{ url('general/walkthrough') }}">Walk Through</a></li>--}}
+            {{--<li><a class="nav-link" href="{{ url('general/highlight') }}">Highlight</a></li>--}}
+            {{--</ul>--}}
+            {{--</li>--}}
+            {{--<li class="nav-item dropdown">--}}
+            {{--<a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i> <span>Companies</span></a>--}}
+            {{--<ul class="dropdown-menu">--}}
+            {{--<li><a class="nav-link" href="{{url('company/community')}}">Community</a></li>--}}
+            {{--<li><a class="nav-link" href="{{url('company/secretariat')}}">Secretariat</a></li>--}}
+            {{--<li><a class="nav-link" href="{{url('company/shop')}}">Shop</a></li>--}}
+            {{--<li><a class="nav-link" href="{{url('company/store')}}">Store</a></li>--}}
+            {{--</ul>--}}
+            {{--</li>--}}
+            {{--</ul>--}}
 
-    <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-      <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-        <i class="fas fa-rocket"></i> Documentation
-      </a>
-    </div>
-  </aside>
+            <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+                <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+                    <i class="fas fa-rocket"></i> Documentation
+                </a>
+            </div>
+        </ul>
+    </aside>
 </div>
 
 @section('script_page')
