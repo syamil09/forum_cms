151dda0738e096cc9f14b9134eb2e1769a723bfb
Abimanyu Wima Putra <kyoabhiem@gmail.com>
2020-05-28 16:10:25 +0700

    update detail event

--- resources/views/app/general/event/detail.blade.php
+++ resources/views/app/general/event/detail.blade.php
@@ -13,6 +13,70 @@
 @endsection
 
 @section('wrap_content')
+    <style>
+        .is-invalid {
+            color: red;
+        }
+
+        .preview-images-zone {
+            width: 100%;
+            border: 1px solid #ddd;
+            min-height: 90px;
+            /* display: flex; */
+            padding: 5px 5px 0px 5px;
+            position: relative;
+            overflow: auto;
+        }
+
+        .preview-images-zone > .preview-image:first-child {
+            position: relative;
+            margin-right: 5px;
+        }
+
+        .preview-images-zone > .preview-image {
+            height: 90px;
+            width: 90px;
+            position: relative;
+            margin-right: 5px;
+            float: left;
+            margin-bottom: 5px;
+        }
+
+        .preview-images-zone > .preview-image > .image-zone {
+            width: 100%;
+            height: 100%;
+        }
+
+        .preview-images-zone > .preview-image > .image-zone > img {
+            width: 100%;
+            height: 100%;
+        }
+
+        .preview-images-zone > .preview-image > .tools-edit-image {
+            position: absolute;
+            z-index: 100;
+            color: #fff;
+            bottom: 0;
+            width: 100%;
+            text-align: center;
+            margin-bottom: 10px;
+            display: none;
+        }
+
+        .preview-images-zone > .preview-image > .image-cancel {
+            font-size: 18px;
+            position: absolute;
+            top: 0;
+            right: 0;
+            font-weight: bold;
+            margin-right: 10px;
+            cursor: pointer;
+            display: none;
+            z-index: 100;
+        }
+
+
+    </style>
 <div class="row">
   <div class="col-12">
       <div class="card">
@@ -33,7 +97,15 @@
               </div>
             </div> -->
             <div class="col-sm-12 col-md-7">
-              <img src="{{$event['image'][0]}}" alt="Thumbnail" class="img-fluid rounded">
+                <div class="preview-images-zone">
+                    @foreach($event['image'] as $i => $image)
+                        <div class="preview-image preview-show-{{$i}}">
+                            <div class="image-cancel" data-no="{{$i}}">x</div>
+                            <div class="image-zone"><img id="pro-img-{{$i}}" src="{{ $image }}"></div>
+                            <input type="text" name="imageView[]" style="display: none"  class="form-control" value="{{ $image }}">
+                        </div>
+                    @endforeach
+                </div>
             </div>
           </div>
           <div class="form-group row mb-4">
@@ -49,16 +121,16 @@
             <!-- <div class="col-sm-12 col-md-7">
               <textarea class="summernote-simple"></textarea>
             </div> -->
-            <!-- <label class="col-form-label text-md-left col-12 col-md-3 col-lg-6">{!! $event['description'] !!}</label> -->
+            <label class="col-form-label text-md-left col-12 col-md-3 col-lg-6">{!! $event['description'] !!}</label>
           </div>
           <div class="form-group row mb-4">
             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3 offset-1">Location : </label>
             <label class="col-form-label text-md-left col-12 col-md-3 col-lg-3">{{$event['location']}}</label>
           </div>
-          <div class="form-group row mb-4">
-            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3 offset-1">Maps : </label>
-            <label class="col-form-label text-md-left col-12 col-md-3 col-lg-3"></label>
-          </div>
+          {{--<div class="form-group row mb-4">--}}
+            {{--<label class="col-form-label text-md-right col-12 col-md-3 col-lg-3 offset-1">Maps : </label>--}}
+            {{--<label class="col-form-label text-md-left col-12 col-md-3 col-lg-3"></label>--}}
+          {{--</div>--}}
                     <div class="form-group row mb-4">
                       <label class="col-form-label text-md-right col-12 col-md-3"></label>
                       <div class="col-sm-12 col-md-7">
